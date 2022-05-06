b24ee4bcb2e2615ec90272b5cc70a6da06745a08
SubashGitLogin <subash007official@gmail.com>
2022-05-06 06:41:34 -0700

    A added some Codes

--- src/org/data/Details.java
+++ src/org/data/Details.java
@@ -2,6 +2,9 @@ package org.data;
 
 public class Details {
 	
+private void caseA() {
+	System.out.println("Case A added");
+}	
 private void tc3() {
 	System.out.println("Test Case 3 Complted");
 }	
@@ -9,6 +12,10 @@ private void tc4() {
 	System.out.println("Test Case 4 Completed");
 }
 public static void main(String[] args) {
+	Details d=new Details();
+	d.caseA();
+	d.tc3();
+	d.tc4();
 	
 	
 	
