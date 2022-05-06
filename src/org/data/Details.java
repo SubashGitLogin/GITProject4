2972cf6960f646fec67349c65ed48006f5d4645f
SubashGitLogin <subash007official@gmail.com>
2022-05-06 03:38:26 -0700

    Codes Added

---
+++ src/org/data/Details.java
@@ -0,0 +1,30 @@
+package org.data;
+
+public class Details {
+	
+private void tc3() {
+	System.out.println("Test Case 3 Complted");
+}	
+private void tc4() {
+	System.out.println("Test Case 4 Completed");
+}
+public static void main(String[] args) {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
